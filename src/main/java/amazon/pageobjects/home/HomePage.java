eefcc044b6b0da43bf2b54df0c77e316be444139
Prasanna Pujar <prasanna4jk@gmail.com>
2020-07-26 22:22:52 +0530

    added method to waitforHomePage to load

--- src/main/java/amazon/pageobjects/home/HomePage.java
+++ src/main/java/amazon/pageobjects/home/HomePage.java
@@ -1,8 +1,12 @@
 package amazon.pageobjects.home;
 
-import amazon.pageobjects.common.BasePage;
+
 import amazon.pageobjects.common.HeaderFooterPage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 /**
  * This page represent the Landing page when you hit amazon.in <br/>
@@ -10,6 +14,11 @@ import org.openqa.selenium.WebDriver;
  * @author prasanna pujar
  */
 public class HomePage extends HeaderFooterPage {
+  private Logger log= LogManager.getLogger(HomePage.class);
+  private static final By amazonLogoAnchor=By.cssSelector("a[href='/ref=nav_logo']");
+  private static final By completePageDiv=By.id("pageContent");
+
+
     /**
      * Initialize the driver object
      *
@@ -19,4 +28,30 @@ public class HomePage extends HeaderFooterPage {
     public HomePage(WebDriver driver) {
         super(driver);
     }
+
+    /**
+     * wait for Home page load by waiting till amazon logo on top left corner to load
+     * and main Div to the page to be loaded up in DOM
+     * max time : 30 seconds
+     * @return : true : if HomePage loads up else false
+     */
+
+    public Boolean waitForHomePageToLoad()
+    {
+        Boolean amazonImage=waitForElementToBeDisplayed(amazonLogoAnchor);
+        log.debug(" amazonImage is displayed "+amazonImage);
+        Boolean completePage=waitForPresenceOfElement(completePageDiv);
+        log.debug(" Complete page is loaded in DIV ");
+        if(amazonImage && completePage)
+        {
+            log.info(" HomePage has loaded ");
+            return true;
+        }else
+        {
+            log.warn(" HomePage not loaded up yet");
+            return false;
+        }
+    }
+
+
 }
