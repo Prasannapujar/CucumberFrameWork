b788dceb16bdab89f03d29a1fd5c484b2bf90b1f
Prasanna Pujar <prasanna4jk@gmail.com>
2020-07-30 23:20:22 +0530

    added step defs

--- src/test/java/stepdefinition/HomePageStepDef.java
+++ src/test/java/stepdefinition/HomePageStepDef.java
@@ -1,11 +1,14 @@
 package stepdefinition;
 
 import amazon.pageobjects.home.HomePage;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+
 
 public class HomePageStepDef extends TestBase {
 
@@ -23,13 +26,50 @@ public class HomePageStepDef extends TestBase {
       {
           HomePage hp= new HomePage(base.driver);
           Assert.assertTrue(hp.waitForHomePageToLoad(), " Wait for Home");
+          base.scenario.log(" Home page is loaded up");
 
       }catch (Exception e)
       {
+          log.error(" problem in step user is at HomePage of amazon ");
           e.printStackTrace();
           throw new Exception();
       }
 
 
+    }
+
+
+    @When("User clicks of SignInTab")
+    public void user_clicks_of_sign_in_tab() throws Exception {
+        try
+        {
+            HomePage hp =new HomePage(base.driver);
+            hp.clickSignInAccountListLink();
+            base.scenario.log(" clicked on Sign in account link");
+
+        }catch (Exception e)
+        {
+            log.error(" Problem in step User clicks of SignInTab ");
+            e.printStackTrace();
+            throw new Exception();
+        }
+
+    }
+
+    @Then("User should Redirected to HomePage")
+    public void userShouldRedirectedToHomePage() throws  Exception {
+
+        try
+        {
+            HomePage hp= new HomePage(base.driver);
+            Assert.assertTrue(hp.waitForHomePageToLoad(), " Wait for Home");
+            base.scenario.log(" Home page is loaded up");
+
+        }catch (Exception e)
+        {
+            log.error(" problem in step user is at HomePage of amazon ");
+            e.printStackTrace();
+            throw new Exception();
+        }
     }
 }
--- src/test/java/stepdefinition/LoginPageStepDef.java
+++ src/test/java/stepdefinition/LoginPageStepDef.java
@@ -1,4 +1,71 @@
 package stepdefinition;
 
+import amazon.pageobjects.authentication.LoginPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.testng.Assert;
+
 public class LoginPageStepDef extends TestBase{
+
+    private Logger log= LogManager.getLogger(LoginPageStepDef.class);
+    private TestBase base;
+    public  LoginPageStepDef(TestBase base)
+    {
+        this.base=base;
+    }
+
+  @Then("SignInPage should be displayed")
+    public  void SignIn_page_shouldbe_displayed() throws  Exception
+  {
+      try
+      {
+          LoginPage lp= new LoginPage(base.driver);
+          Assert.assertTrue(lp.waitForLoginPage(), " wait for login page");
+          base.scenario.log(" Signin page is displayed ");
+      }catch(Exception e)
+      {
+          log.error(" problem in step SignInPage should be displayed");
+          e.printStackTrace();
+          throw  new Exception();
+      }
+
+  }
+
+    @Given("user enter Email id as {string}")
+    public void userEnterEmailIdAs(String  email) throws  Exception {
+        try
+        {
+            LoginPage lp= new LoginPage(base.driver);
+            lp.enterEmailId(email);
+            base.scenario.log(" Email id is entered");
+        }catch (Exception e)
+        {
+            log.error(" problem in step user enter Email id as {string}");
+            e.printStackTrace();
+            throw  new Exception();
+
+        }
+    }
+
+    @When("user enter password as {String}")
+    public void userEnterPasswordAsPmpAmazon$(String pwd) throws  Exception {
+
+        try
+        {
+            LoginPage lp= new LoginPage(base.driver);
+            lp.enterPassword(pwd);
+            base.scenario.log(" password entered ");
+        }catch (Exception e)
+        {
+            log.error(" problem in step user enter Email id as {string}");
+            e.printStackTrace();
+            throw  new Exception();
+
+        }
+    }
+
 }
